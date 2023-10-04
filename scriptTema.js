045553fdb3b18f8a200d3bcb7aa81536e9313938
HenriBarboza <henr.fe7@gmail.com>
2023-10-04 14:40:52 -0300

    Algumas pequenas mundanças
    Alteração no estilo dos botões de tema, adicionando novas funcionalidades a eles.

--- scriptTema.js
+++ scriptTema.js
@@ -1,6 +1,7 @@
 const stylePadraoButton = document.getElementById('stylePadrao');
 const styleClaroButton = document.getElementById('styleClaro');
 const styleEscuroButton = document.getElementById('styleEscuro');
+const btnSelected = document.querySelectorAll('.btn-theme');
 const themeStyle = document.getElementById('theme-style');
 
 // Funções para alternar temas
@@ -8,6 +9,19 @@ function setTheme(theme) {
     themeStyle.href = `${theme}.css`;
     localStorage.setItem('selectedTheme', theme);
 }
+
+btnSelected.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        // Remove a classe "selecionado" de todos os botões
+        btnSelected.forEach((b) => b.classList.remove('selecionado'));
+        
+        // Adiciona a classe "selecionado" ao botão clicado
+        btn.classList.add('selecionado');
+
+        // Salva o ID do botão selecionado no localStorage
+        localStorage.setItem('ultimoBotaoSelecionado', btn.id);
+    });
+});
 
 stylePadraoButton.addEventListener('click', () => setTheme('stylePadrao'));
 styleClaroButton.addEventListener('click', () => setTheme('styleClaro'));
@@ -19,4 +33,12 @@ if (savedTheme) {
     setTheme(savedTheme); // Aplicar o tema anteriormente selecionado ao carregar a página
 } else {
     setTheme('stylePadrao'); // Tema padrão se nenhum tema foi selecionado anteriormente
+}
+
+const ultimoBotaoSelecionado = localStorage.getItem('ultimoBotaoSelecionado');
+if (ultimoBotaoSelecionado) {
+    const botaoSelecionado = document.getElementById(ultimoBotaoSelecionado);
+    if (botaoSelecionado) {
+        botaoSelecionado.click();
+    }
 }
